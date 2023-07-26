e500a6d80383b75bb76e87950c1584be7dd4888f
chandkommanaboyina <99801710+chandkommanaboyina@users.noreply.github.com>
2023-07-26 13:49:47 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+apt update
+apt upgrade
+wget https://go.dev/dl/go1.20.6.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.6.linux-amd64.tar.gz
+
+cat << EOF >> ~/.bashrc
+
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$HOME/.local/bin:$PATH
+
+EOF
+
+source ~/.profile
+
+go install -v github.com/owasp-amass/amass/v4/...@master
+
+
+
+
